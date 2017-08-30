f46b665b4b6a468181f5dda384f898ee70e5a9b7
Jamie Folsom <jamiefolsom@gmail.com>
2017-08-30 17:04:16 -0400

    Add Jekyll site

---
+++ _tools/generate_pyramid_tiffs.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+INPUT_DIR = 'tiffs'
+OUTPUT_DIR = 'ptifs'
+
+source_files = Dir["#{INPUT_DIR}/*"]
+
+source_files.each { |source_file| 
+  output_file = "#{OUTPUT_DIR}/#{source_file.match(/\/(\S+)/)[1]}"
+  
+  system("convert #{source_file} -define tiff:tile-geometry=128x128 -compress jpeg 'ptif:#{output_file}'")
+}
