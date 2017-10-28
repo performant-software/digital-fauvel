e42b1663a3aa3f387961c8ffb21802dc4a3a96be
Andy Stuhl <andykstuhl@gmail.com>
2017-10-28 15:50:05 -0500

    render annotation text and connect layers to toggle menu
    overlay text on annotations, connect to text layer menu
    
    use standard mirador event
    
    update languages and hide annotation hit boxes
    
    add languages to manifest

--- js/MiradorTextLayer.js
+++ js/MiradorTextLayer.js
@@ -4,14 +4,25 @@ var MiradorTextLayer = {
     locales: {
         en: {
             translation: {
-                'button-tooltip': 'Show Text Layer'
+                'button-tooltip': 'Show Text Layer',
+                'text-layer-none': 'None',
+                'text-layer-en': 'English',
+                'text-layer-fr': 'French',
+                'text-layer-fro': 'Original'
             }
         }
     },
 
     template: Mirador.Handlebars.compile([
-        '<a href="javascript:;" class="mirador-btn mirador-icon-disable-zoom contained-tooltip" title="{{t "button-tooltip"}}">',
+        '<a href="javascript:;" class="mirador-btn mirador-icon-text-layer" title="{{t "button-tooltip"}}">',
             '<i class="fa fa-align-left fa-lg fa-fw"></i>',
+            '<i class="fa fa-caret-down"></i>',
+            '<ul class="dropdown text-layer-list">',
+              '<li class="text-layer-none"><i class="fa fa-ban fa-lg fa-fw"></i> {{t "text-layer-none"}}</li>',
+              '<li class="text-layer-en"><i class="fa fa-font fa-lg fa-fw"></i> {{t "text-layer-en"}}</li>',
+              '<li class="text-layer-fr"><i class="fa fa-font fa-lg fa-fw"></i> {{t "text-layer-fr"}}</li>',
+              '<li class="text-layer-fro"><i class="fa fa-font fa-lg fa-fw"></i> {{t "text-layer-fro"}}</li>',
+            '</ul>',
         '</a>'
     ].join('')),
 
@@ -40,6 +51,12 @@ var MiradorTextLayer = {
 
             /* 1. Override methods and register (and document!) new ones. */
 
+            $.Window.prototype.textLayerKeys = [
+              'en',
+              'fr',
+              'fro'
+            ];
+
             $.Window.prototype.listenForActions = function() {
                 var _this = this;
                 listenForActions.apply(this, arguments);
@@ -47,7 +64,49 @@ var MiradorTextLayer = {
                 this.eventEmitter.subscribe('focusUpdated' + this.id, function(event, focusState) {
                     // triggered when toggling viewing states or changing the current canvas
                     // a new OSD will be created, so just de-select the button
-                    _this.element.find('.mirador-icon-disable-zoom').removeClass('selected');
+                    _this.element.find('.mirador-icon-text-layer').removeClass('selected');
+                });
+
+                var annotationTextPad = 20,
+                  defaultLayer = 'en',
+                  backdropColor = '#E6D9B7',
+                  backdropOpacity = 0.7;
+                var timeout;
+                this.currentTextLayer = defaultLayer;
+                this.eventEmitter.subscribe('annotationsRendered.' + _this.id, function() {
+                  if (timeout) clearTimeout(timeout);
+                  timeout = setTimeout(function() {
+                    var items = _this.focusModules.ImageView.annotationsLayer.drawTool.annotationsToShapesMap;
+                    if (items) {
+                      for (key in items) {
+                        var item = items[key][0];
+                        if (item.data && item.data.annotation && item.data.annotation.resource) {
+                          var resource = item.data.annotation.resource;
+                          var bounds = item.bounds;
+                          if (resource.length > 0) resource = resource[0];
+                          if (resource.chars && bounds) {
+                            var r = new paper.Path.Rectangle({
+                              point: [bounds.x, bounds.y],
+                              size: [bounds.width, bounds.height],
+                              fillColor: backdropColor,
+                              opacity: backdropOpacity,
+                              name: item.name + '_' + resource.language + '_bgrect',
+                              visible: resource.language == _this.currentTextLayer
+                            });
+                            var t = new paper.PointText();
+                            t.point = new paper.Point(bounds.x + annotationTextPad, bounds.y + bounds.height - annotationTextPad);
+                            t.content = resource.chars;
+                            t.fontSize = (bounds.height - 2 * annotationTextPad) + "px";
+                            t.name = item.name + '_' + resource.language + '_text';
+                            t.visible = (resource.language == _this.currentTextLayer);
+                            if (t.bounds.width + annotationTextPad > r.bounds.width) r.bounds.width = t.bounds.width + annotationTextPad * 2.0;
+                            item.visible = false;
+                          }
+                        }
+                      }
+                    }
+                    paper.view.draw();
+                  }, 300);
                 });
             };
 
@@ -57,20 +116,60 @@ var MiradorTextLayer = {
 
                 // add button (the compiled template) to the DOM
                 this.element.find('.window-manifest-navigation').prepend(self.template());
-                // add click handler for the new button
-                this.element.find('.mirador-icon-disable-zoom').on('click', function(event) {
-                    _this.showTextLayer(this, !_this.textLayerShown);
+
+                _this.bindTextLayerNavigation();
+            };
+
+            $.Window.prototype.bindTextLayerNavigation = function() {
+                var _this = this;
+
+                this.element.find('.mirador-icon-text-layer').on('mouseenter',
+                  function() {
+                    _this.element.find('.text-layer-list').stop().slideFadeToggle(300);
+                  }).on('mouseleave',
+                  function() {
+                    _this.element.find('.text-layer-list').stop().slideFadeToggle(300);
+                  });
+
+                this.element.find('.text-layer-none').on('click', function() {
+                  var view = _this.focusModules.ImageView;
+                  if (view.hud.annoState.current === 'none') {
+                    view.hud.annoState.startup(this);
+                  }
+                  if (view.hud.annoState.current !== 'off') {
+                    view.forceShowControls = false;
+                    view.hud.annoState.displayOff(this);
+                    view.annotationState = 'off';
+                  }
+                  _this.currentTextLayer = 'none';
+                });
+
+                _this.textLayerKeys.forEach(function(layerType) {
+                  _this.bindTextLayerClick(layerType);
                 });
             };
 
-            /*
-             * Mirador.Window.prototype.showTextLayer
-             *
-             * Shows and hides the textLayer
-             */
-            $.Window.prototype.showTextLayer = function() {
-                console.log("showed the text layer");
+            $.Window.prototype.bindTextLayerClick = function(layerType) {
+              var _this = this;
+              this.element.find('.text-layer-' + layerType).on('click', function() {
+                var view = _this.focusModules.ImageView;
+                if (view.hud.annoState.current === 'none') {
+                  view.hud.annoState.startup(this);
+                }
+                if (view.hud.annoState.current === 'off') {
+                  view.hud.annoState.displayOn(this);
+                  view.annotationState = 'on';
+                }
+                paper.project.getItems({name: new RegExp('_(' + _this.textLayerKeys.join('|') + ')_')}).forEach(function(item) {
+                  item.visible = false;
+                });
+                paper.project.getItems({name: new RegExp('_' + layerType + '_')}).forEach(function(item) {
+                  item.visible = true;
+                });
+                _this.currentTextLayer = layerType;
+              });
             };
+
 
             /* 2. Override the constructor. */
 
@@ -94,7 +193,7 @@ var MiradorTextLayer = {
                 var constructor = $[viewType],
                     prototype = $[viewType].prototype,
                     listenForActions = $[viewType].prototype.listenForActions;
-            
+
                 /* 1. */
 
                 $[viewType].prototype.listenForActions = function() {
