b8a43dd38a61c0d89e9e6fa2b3350d1f22da5421
Jamie Folsom <jamiefolsom@gmail.com>
2018-06-28 23:50:11 -0400

    Reconverted all images.

--- tools/generate_pyramid_tiffs.rb
+++ tools/generate_pyramid_tiffs.rb
@@ -5,8 +5,8 @@ OUTPUT_DIR = 'ptifs'
 
 source_files = Dir["#{INPUT_DIR}/*"]
 
-source_files.each { |source_file| 
+source_files.each { |source_file|
   output_file = "#{OUTPUT_DIR}/#{source_file.match(/\/(\S+)/)[1]}"
-  
-  system("convert #{source_file} -define tiff:tile-geometry=128x128 -compress jpeg 'ptif:#{output_file}'")
+
+  system("convert #{source_file} -define tiff:tile-geometry=1024x1024 -depth 8 -compress jpeg 'ptif:#{output_file}'")
 }
